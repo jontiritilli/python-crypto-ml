7d77959e1eedd2a937f15330cd457322851b952a
Jonathan Tiritilli <jonathantiritilli@gmail.com>
2019-06-08 10:38:12 -0700

    organizing

--- ml/cryptornn.py
+++ ml/cryptornn.py
@@ -4,6 +4,12 @@ SEQ_LEN = 60  # how long of a preceeding sequence to collect for RNN
 FUTURE_PERIOD_PREDICT = 3  # how far into the future are we trying to predict?
 RATIO_TO_PREDICT = "XRP-USD"
 
+def classify(current, future):
+    if float(future) > float(current):
+        return 1
+    else:
+        return 0
+
 main_df = pd.DataFrame() # begin empty
 
 ratios = ["BTC-USD", "LTC-USD", "XRP-USD", "ETH-USD"] # the 4 ratios we want to consider
@@ -26,12 +32,6 @@ for ratio in ratios:  # begin iteration
 
 main_df.fillna(method="ffill", inplace=True)  # if there are gaps in data, use previously known values
 main_df.dropna(inplace=True)
-
-def classify(current, future):
-    if float(future) > float(current):
-        return 1
-    else:
-        return 0
 
 main_df['future'] = main_df[f'{RATIO_TO_PREDICT}_close'].shift(-FUTURE_PERIOD_PREDICT)
 main_df['target'] = list(map(classify, main_df[f'{RATIO_TO_PREDICT}_close'], main_df['future']))
