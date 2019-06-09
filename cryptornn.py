77c21975c2c6e2c6cd181f11c9bdb383503ef817
Jonathan <jonathantiritilli@gmail.com>
2019-06-09 16:16:36 -0700

    debug issue, remove ltc data as a result. Add training simulation

--- cryptornn.py
+++ cryptornn.py
@@ -1,8 +1,20 @@
 import pandas as pd
+from collections import deque
+import random
+from datetime import datetime
+import time
+import numpy as np
+from sklearn import preprocessing
 
-SEQ_LEN = 60  # how long of a preceeding sequence to collect for RNN
-FUTURE_PERIOD_PREDICT = 3  # how far into the future are we trying to predict?
+# CONSTANTS
+
+SEQ_LEN = int(60)  # how long of a preceeding sequence to collect for RNN
+FUTURE_PERIOD_PREDICT = int(3)  # how far into the future are we trying to predict?
 RATIO_TO_PREDICT = "XRP-USD"
+
+# END CONSTANTS
+
+# FUNCTION DECLERATIONS
 
 def classify(current, future):
     if float(future) > float(current):
@@ -10,15 +22,70 @@ def classify(current, future):
     else:
         return 0
 
-main_df = pd.DataFrame() # begin empty
+def preprocess_df(df):
+    df = df.drop("future", 1)  # don't need this anymore.
 
-ratios = ["BTC-USD", "LTC-USD", "XRP-USD", "ETH-USD"] # the 4 ratios we want to consider
+    for col in df.columns:  # go through all of the columns
+        print(col)
+        if col != "target":  # normalize all ... except for the target itself!
+            df[col] = df[col].pct_change()  # pct change "normalizes" the different currencies (each crypto coin has vastly diff values, we're really more interested in the other coin's movements)
+            df.dropna(inplace=True)  # remove the nans created by pct_change
+            df[col] = preprocessing.scale(df[col].values)  # scale between 0 and 1.
 
+    df.dropna(inplace=True)  # cleanup again... jic.
+
+    sequential_data = []  # this is a list that will CONTAIN the sequences
+    prev_days = deque(maxlen=SEQ_LEN)  # These will be our actual sequences. They are made with deque, which keeps the maximum length by popping out older values as new ones come in
+
+    for i in df.values:  # iterate over the values
+        prev_days.append([n for n in i[:-1]])  # store all but the target
+        if len(prev_days) == SEQ_LEN:  # make sure we have 60 sequences!
+            sequential_data.append([np.array(prev_days), i[-1]])  # append those bad boys!
+
+    random.shuffle(sequential_data)  # shuffle for good measure.
+
+    buys = []  # list that will store our buy sequences and targets
+    sells = []  # list that will store our sell sequences and targets
+
+    for seq, target in sequential_data:  # iterate over the sequential data
+        if target == 0:  # if it's a "not buy"
+            sells.append([seq, target])  # append to sells list
+        elif target == 1:  # otherwise if the target is a 1...
+            buys.append([seq, target])  # it's a buy!
+
+    random.shuffle(buys)  # shuffle the buys
+    random.shuffle(sells)  # shuffle the sells!
+
+    lower = min(len(buys), len(sells))  # what's the shorter length?
+
+    buys = buys[:lower]  # make sure both lists are only up to the shortest length.
+    sells = sells[:lower]  # make sure both lists are only up to the shortest length.
+
+    sequential_data = buys+sells  # add them together
+    random.shuffle(sequential_data)  # another shuffle, so the model doesn't get confused with all 1 class then the other.
+
+    X = []
+    y = []
+
+    for seq, target in sequential_data:  # going over our new sequential data
+        X.append(seq)  # X is the sequences
+        y.append(target)  # y is the targets/labels (buys vs sell/notbuy)
+
+    return np.array(X), y  # return X and y...and make X a numpy array!
+
+# END FUNCTION DECLERATIONS
+
+main_df = pd.DataFrame() # new empty dataframe
+
+# ratios = ["BTC-USD", "ETH-USD", "XRP-USD", "LTC-USD"] # the 4 ratios we want to consider
+ratios = ["BTC-USD", "ETH-USD", "XRP-USD"] # the 4 ratios we want to consider
+
+# generate set of data and merge each ratio into single csv
 for ratio in ratios:  # begin iteration
     print(ratio)
     dataset = f'./data/{ratio}.csv'  # get the full path to the file.
     df = pd.read_csv(dataset, names=['time', 'symbol', 'open', 'high', 'low', 'close', 'volume', 'vol_usd'])  # read in specific file
-
+    df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %I-%p')
     # rename volume and close to include the ticker so we can still which close/volume is which:
     df.rename(columns={"close": f"{ratio}_close", "volume": f"{ratio}_volume"}, inplace=True)
 
@@ -35,11 +102,17 @@ main_df.dropna(inplace=True)
 
 main_df['future'] = main_df[f'{RATIO_TO_PREDICT}_close'].shift(-FUTURE_PERIOD_PREDICT)
 main_df['target'] = list(map(classify, main_df[f'{RATIO_TO_PREDICT}_close'], main_df['future']))
-
+main_df = main_df[:-3]
 times = sorted(main_df.index.values)  # get the times
 last_5pct = sorted(main_df.index.values)[-int(0.05*len(times))]  # get the last 5% of the times
-
+# print(f'last 5% {last_5pct}')
 validation_main_df = main_df[(main_df.index >= last_5pct)]  # make the validation data where the index is in the last 5%
 main_df = main_df[(main_df.index < last_5pct)]  # now the main_df is all the data up to the last 5%
+# print(validation_main_df)
+train_x, train_y = preprocess_df(main_df)
+validation_x, validation_y = preprocess_df(validation_main_df)
 
-print(main_df.head())
+print(f"train data: {len(train_x)} validation: {len(validation_x)}")
+print(f"Dont buys: {train_y.count(0)}, buys: {train_y.count(1)}")
+print(f"VALIDATION Dont buys: {validation_y.count(0)}, buys: {validation_y.count(1)}")
+
